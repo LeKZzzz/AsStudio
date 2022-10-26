1bf3eb477e40405fc69c3198b28459f1a43b9172
LeK <1148579949@qq.com>
2022-10-27 00:18:50 +0800

    在通用工具库中添加了Log类以实现日志管理

---
+++ core/danmaku.py
@@ -0,0 +1,3 @@
+# -*- coding: UTF-8 -*- 
+# Creator：LeK
+# Date：2022/10/26
--- core/utils.py
+++ core/utils.py
@@ -10,6 +10,8 @@
 import os
 import json
 from bilibili_api import live
+import time
+import logging
 
 
 def get_path(field: str):
@@ -22,13 +24,13 @@ def get_path(field: str):
     Returns:
         配置文件路径
     """
-    pathindex = os.path.join(os.path.dirname(__file__), "confpath.json")
+    pathindex = os.path.join(os.path.dirname(__file__), "path.json")
     if os.path.exists(pathindex):
         with open(pathindex, encoding="utf8") as f:
             return os.path.join(os.path.dirname(__file__), '..', json.loads(f.read())[field])
 
 
-async def get_room_info(room_id):
+async def get_room_info(room_id: int):
     """
     直播间信息获取
     :param room_id: 房间号
@@ -42,3 +44,69 @@ async def get_room_info(room_id):
     result['nickname'] = nickname
     result['roomtitle'] = roomtitle
     return result
+
+
+class Log:
+    """
+    日志类
+    """
+
+    def __init__(self, module: str):
+        """
+        初始化日志类
+        :param module:调用的模块名，用于生成日志文件路径
+        """
+        self.pretime = self.curtime = time.localtime()  # 时间检测
+        self.prepath = get_path(module)
+        self.filename = self.prepath + str(self.curtime[0]) + '-' + str(self.curtime[1]) + '-' + str(
+            self.curtime[2]) + '.txt'  # 日志文件的绝对路径
+        # 初始化Logger对象
+        self.logger = logging.getLogger(name='module')
+        self.logger.setLevel(logging.DEBUG)
+        if not os.path.exists(self.filename):  # 检查日志文件
+            self.file = open(self.filename, 'w')
+            self.file.close()
+        self.handler = logging.FileHandler(filename=self.filename, mode='a', encoding='utf-8')
+        self.handler.setLevel(logging.INFO)
+        self.formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+        self.handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.handler)
+
+    def info(self, info: str):
+        """
+        生成正常运行日志
+        :param info:日志信息
+        """
+        Log.filecheck(self)
+        self.logger.info(info)
+
+    def exception(self):
+        """
+        生成错误运行日志
+        """
+        Log.filecheck(self)
+        self.logger.exception('Exception occurred')
+
+    def filecheck(self):
+        """
+        以日为单位，检查是否需要新建日志文件
+        """
+        self.curtime = time.localtime()
+        if self.curtime[2] > self.pretime[2]:
+            self.filename = self.prepath + str(self.curtime[0]) + '-' + str(self.curtime[1]) + '-' + str(
+                self.curtime[2]) + '.txt'
+            Log.sethandler(self)
+            self.pretime = self.curtime
+
+    def sethandler(self):
+        """
+        新建日志文件，重新设置处理器
+        """
+        self.file = open(self.filename, 'w')
+        self.file.close()
+        self.logger.removeHandler(self.handler)
+        self.handler = logging.FileHandler(filename=self.filename, mode='a', encoding='utf-8')
+        self.handler.setLevel(logging.INFO)
+        self.formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+        self.handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.handler)
